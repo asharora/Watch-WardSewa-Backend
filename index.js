8d30896854223199ae0e0e6582da466705614328
asharora <ashisharora111122@gmail.com>
2021-06-06 12:26:04 +0530

    my changes

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const movieRouter = require('./router')
 const app = express()
-const apiPort = 6000
-
+const apiPort = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
 
+// app.get('/', function(req, res) {
+//  res.render('Hello World');
+// });
 const db='mongodb+srv://watchward:watch%401234@cluster0.2x0xh.mongodb.net/Duties';
 
 mongodb.connect(db,{ useNewUrlParser: true ,useUnifiedTopology:true}).then((val)=>{
